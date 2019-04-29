8fb0984fa80183204172843c76d832298cba3ed2
Zhongliang <zhongliang@huodongxing.com>
2019-04-29 17:52:35 +0800

    update

--- maven1/src/main/java/WordCountApplication.java
+++ maven1/src/main/java/WordCountApplication.java
@@ -22,7 +22,8 @@ public class WordCountApplication {
     public static void main(final String[] args) throws Exception {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-broker1:9092");
+//        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-broker1:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.95:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
