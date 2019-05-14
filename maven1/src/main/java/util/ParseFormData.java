31e816d6f9df801ab8199f503136a5d5719522d0
Zhongliang <zhongliang@huodongxing.com>
2019-05-14 19:02:45 +0800

    优化

--- maven1/src/main/java/util/ParseFormData.java
+++ maven1/src/main/java/util/ParseFormData.java
@@ -1,36 +1,32 @@
 package util;
 
-
-import com.alibaba.fastjson.JSONArray;
 import model.FormData;
-
-import java.util.LinkedHashMap;
-import java.util.List;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * @Author: zl
  * @Date: 2019/5/14 11:10
+ * 解析报名表单中用户的公司、职位、学历
  */
 public class ParseFormData {
 
     /**
      * 报名表单是一条记录一条记录解析的
      */
-    private static FormData parseBizExtra(String bizExtra) {
+    public static FormData parseBizExtra(String bizExtra) {
         FormData formData = new FormData();
         if (null != bizExtra && bizExtra.length() > 10) {
             //[{"Key":"I_10001","Value":"阿里巴巴"},{"Key":"I_10002","Value":"研发中心"},{"Key":"I_10003","Value":"数据分析师"}]
 
             try {
-                JSONArray jsonArray = JSONArray.parseArray(bizExtra);
-                List<LinkedHashMap> arr = jsonArray.toJavaList(LinkedHashMap.class);
-
+                JSONArray arr = JSONArray.parseArray(bizExtra);
                 if (arr != null && arr.size() > 0) {
-                    LinkedHashMap obj = null;
+                    JSONObject obj = null;
                     String key = null;
                     String val = null;
                     for (int i = 0; i < arr.size(); i++) {
-                        obj = arr.get(i);
+                        obj = JSONObject.parseObject(arr.get(i).toString());
                         key = obj.get("Key").toString();
                         val = obj.get("Value").toString();
                         if (key != null && key.contains("I_100") && val != null && val.length() > 0) {
@@ -88,3 +84,4 @@ public class ParseFormData {
         return formData;
     }
 }
+
