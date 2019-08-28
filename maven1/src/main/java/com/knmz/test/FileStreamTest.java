ff67e34f5b5998171695b660e804525f2fc4dbcd
Zhongliang <zhongliang@huodongxing.com>
2019-08-28 15:43:20 +0800

    update

--- maven1/src/main/java/com/knmz/test/FileStreamTest.java
+++ maven1/src/main/java/com/knmz/test/FileStreamTest.java
@@ -12,11 +12,33 @@ import java.util.List;
 public class FileStreamTest {
     public static void main(String[] args) {
         // 读txt文件
-        StringBuilder stringBuilder = FileStreamUtil.readFile();
+        String pathName = "E:\\Program Files\\image\\phone.txt";
+//        String pathName = "E:\\Program Files\\image\\email.txt";
+        StringBuilder stringBuilder = FileStreamUtil.readFile(pathName);
         List<String> stringList = Arrays.asList(stringBuilder.toString().split(","));
-        System.out.println(stringList.size());
+        System.out.println("读到个数：" + stringList.size());
 
         // 写txt文件
-//        FileStreamUtil.writeFile();
+        String pathNameWrite = "E:\\Program Files\\image\\output.txt";
+//        String context = "写文件啦\r\n";
+//        FileStreamUtil.writeFile(pathNameWrite, context);
+
+        StringBuilder stringBuilderWrite = new StringBuilder();
+        if (stringList.size() > 0) {
+            int i = 0;
+            for (String data : stringList) {
+                if (i >= 900 && null != data) {
+                    stringBuilderWrite.append(data);
+                    stringBuilderWrite.append(",");
+                }
+                i++;
+            }
+            //去掉末尾的分隔符
+            stringBuilderWrite.deleteCharAt(stringBuilderWrite.length() - 1);
+            FileStreamUtil.writeFile(pathNameWrite, stringBuilderWrite.toString());
+
+            List<String> stringWriteList = Arrays.asList(stringBuilderWrite.toString().split(","));
+            System.out.println("写到个数：" + stringWriteList.size());
+        }
     }
 }
--- maven1/src/main/java/com/knmz/util/FileStreamUtil.java
+++ maven1/src/main/java/com/knmz/util/FileStreamUtil.java
@@ -9,13 +9,11 @@ import java.io.*;
 public class FileStreamUtil {
     /**
      * 读入TXT文件
+     *
+     * @param pathName 绝对路径或相对路径都可以，写入文件时演示相对路径,读取以上路径的input.txt文件
      */
-    public static StringBuilder readFile() {
+    public static StringBuilder readFile(String pathName) {
         StringBuilder stringBuilder = new StringBuilder();
-        // 绝对路径或相对路径都可以，写入文件时演示相对路径,读取以上路径的input.txt文件
-        String pathName = "E:\\Program Files\\image\\phone.txt";
-//        String pathName = "E:\\Program Files\\image\\email.txt";
-
         // 不关闭文件会导致资源的泄露，读写文件都同理;Java7的try-with-resources可以优雅关闭文件，异常时自动关闭文件；
         try (FileReader reader = new FileReader(pathName);
              // 建立一个对象，它把文件内容转成计算机能读懂的语言
@@ -26,7 +24,6 @@ public class FileStreamUtil {
             while ((line = br.readLine()) != null) {
                 // 一次读入一行数据
                 stringBuilder.append(line);
-//                System.out.println(line);
             }
         } catch (IOException e) {
             // 防止文件建立或读取失败，用catch捕捉错误并打印，也可以throw;
@@ -37,19 +34,20 @@ public class FileStreamUtil {
 
     /**
      * 写入TXT文件
+     *
+     * @param pathName 文件名
+     * @param context  文件内容
      */
-    public static void writeFile() {
+    public static void writeFile(String pathName, String context) {
         try {
             // 相对路径，如果没有则要建立一个新的output.txt文件
-            File writeName = new File("E:\\Program Files\\image\\output.txt");
+            File writeName = new File(pathName);
             // 创建新文件,有同名的文件的话直接覆盖
             writeName.createNewFile();
             try (FileWriter writer = new FileWriter(writeName);
                  BufferedWriter out = new BufferedWriter(writer)
             ) {
-                // \r\n即为换行
-                out.write("我会写入文件啦1\r\n");
-                out.write("我会写入文件啦2\r\n");
+                out.write(context);
                 // 把缓存区内容压入文件
                 out.flush();
             }
