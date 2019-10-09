ad00403332518415eff6cdaca8e11089f6c76d36
Zhongliang <zhongliang@huodongxing.com>
2019-10-09 11:36:04 +0800

    SHA256加密

--- maven1/src/main/java/com/knmz/test/UtilsTest.java
+++ maven1/src/main/java/com/knmz/test/UtilsTest.java
@@ -1,6 +1,7 @@
 package com.knmz.test;
 
 import com.knmz.util.PinyinTool;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
 /**
@@ -18,6 +19,18 @@ public class UtilsTest {
     public void pingYinTest() {
         String pingYin = PinyinTool.toPinYin("李白", "-", PinyinTool.Type.UPPERCASE);
         System.out.println("测试拼音转化结果为：" + pingYin);
+    }
+
+    /**
+     * 测试sha256加密，SHA256加密后是256位，也就是64个字符。
+     * @author zl
+     *
+     */
+    @Test
+    public void shaTest() {
+        String str = "this is a test string";
+        String sha = DigestUtils.sha256Hex(str).toUpperCase();
+        System.out.println(sha);
     }
 
 }
