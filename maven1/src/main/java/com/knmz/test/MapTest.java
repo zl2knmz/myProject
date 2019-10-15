34b4f0fed7a9fad7d3d981eca4f7e9cded9da135
Zhongliang <zhongliang@huodongxing.com>
2019-10-15 17:30:28 +0800

    ConcurrentHashMap实现缓存，有缺陷。

--- maven1/src/main/java/com/knmz/test/MapTest.java
+++ maven1/src/main/java/com/knmz/test/MapTest.java
@@ -1,10 +1,14 @@
 package com.knmz.test;
 
+import com.knmz.util.MapCacheSingleton;
 import com.knmz.util.Utils;
 import org.junit.Test;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,6 +33,23 @@ public class MapTest {
         }
         FileOutputStream out = new FileOutputStream("E:\\Program Files\\image\\test.txt");
         out.write(Utils.getObjectMapper().writeValueAsBytes(user));
+    }
+
+    /**
+     * 测试 ConcurrentHashMap实现缓存
+     */
+    @Test
+    public void testMapCacheSingleton() throws IOException {
+        MapCacheSingleton mapCacheSingleton = MapCacheSingleton.getInstance();
+        mapCacheSingleton.set("key", "value");
+        String str = mapCacheSingleton.get("key") == null ? null : mapCacheSingleton.get("key").toString();
+        System.out.println(str);
+
+        long time = LocalDateTime.now().plusMinutes(1L).toInstant(ZoneOffset.of("+8")).toEpochMilli();;
+        long time1 = Instant.now().minusSeconds(100L).getEpochSecond();
+        mapCacheSingleton.set("key1", "value-time", time1);
+        String str1 = mapCacheSingleton.get("key1") == null ? null : mapCacheSingleton.get("key1").toString();
+        System.out.println(str1);
     }
 
 }
