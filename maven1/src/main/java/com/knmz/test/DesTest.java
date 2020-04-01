592c71d68c2fd860007850861321945b73fe75e5
Zhongliang <zhongliang@huodongxing.com>
2020-04-01 16:05:20 +0800

    EDS对称加密

---
+++ maven1/src/main/java/com/knmz/test/DesTest.java
@@ -0,0 +1,37 @@
+package com.knmz.test;
+
+import com.knmz.util.DesUtils;
+import org.junit.Test;
+
+/**
+ * @author zl
+ * @date 2020/4/1 15:37
+ */
+public class DesTest {
+    /**
+     * 加密解密的密钥
+     */
+    private final String password = "hdx.20201024";
+
+    @Test
+    public void testDesEncode() {
+        String phoneNum = "18770913915";
+        String enPhoneNum = DesUtils.encrypt(password, phoneNum);
+        System.out.println(enPhoneNum);
+
+        String email = "837870929@qq.com";
+        String enEmail = DesUtils.encrypt(password, email);
+        System.out.println(enEmail);
+    }
+
+    @Test
+    public void testDesDecode() {
+        String enPhoneNum = "/TqXMHpr11DVPF3NgMeyGA==";
+        String dePhoneNum = DesUtils.decrypt(password, enPhoneNum);
+        System.out.println(dePhoneNum);
+
+        String enEmail = "U/ZIJ5WDW5CxLd7ZQakdgAxGqQ+A3Hmh";
+        String deEmail = DesUtils.decrypt(password, enEmail);
+        System.out.println(deEmail);
+    }
+}
