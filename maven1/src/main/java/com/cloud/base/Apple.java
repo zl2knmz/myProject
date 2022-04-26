726904009d62b1b33c95b6f5501d82e4a232cb70
Zhongliang <837870929@qq.com>
2022-04-26 23:25:36 +0800

    1

---
+++ maven1/src/main/java/com/cloud/base/Apple.java
@@ -0,0 +1,8 @@
+package com.cloud.base;
+
+/**
+ * @author zl
+ * @date 2022/4/25 23:34
+ */
+public class Apple extends Fruit {
+}
---
+++ maven1/src/main/java/com/cloud/base/Fruit.java
@@ -0,0 +1,8 @@
+package com.cloud.base;
+
+/**
+ * @author zl
+ * @date 2022/4/25 23:33
+ */
+public class Fruit {
+}
