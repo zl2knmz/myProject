c4c40b5fca4026a244237aaca3662ba59e75508d
zl <837870929@qq.com>
2022-05-06 11:53:29 +0800

    no message

--- maven1/src/main/java/com/cloud/meiju/demo3/Test03.java
+++ maven1/src/main/java/com/cloud/meiju/demo3/Test03.java
@@ -6,14 +6,29 @@ package com.cloud.meiju.demo3;
  */
 public class Test03 {
     public static void main(String[] args) {
-        System.out.println("---------------获取枚举--------------------");
+        System.out.println("--------------EnumsUtil-获取枚举--------------------");
         Constants.LiveWatermark_Enum clearEnum = EnumsUtil.valueOf(Constants.LiveWatermark_Enum.class, "0");
         System.out.println(clearEnum);
 
-        System.out.println("---------------获取枚举集合--------------------");
+        System.out.println("--------------EnumsUtil-获取枚举集合--------------------");
         IEnum[] values = EnumsUtil.values(Constants.LiveWatermarkRule_Enum.class);
         for (IEnum value : values) {
             System.out.println(value);
         }
+
+        System.out.println("---------------枚举常用方法------valueOf--------------");
+        Constants.LiveWatermark_Enum enumMethod1 = Constants.LiveWatermark_Enum.valueOf("CLEAR_WATER_MARK");
+        System.out.println(enumMethod1);
+
+        System.out.println("---------------枚举常用方法-----values---------------");
+        IEnum[] enumMethod2 = Constants.LiveWatermark_Enum.values();
+        for (IEnum iEnum : enumMethod2) {
+            System.out.println(iEnum);
+        }
+
+        System.out.println("---------------枚举常用方法----toString---返回当前枚举类对象常量的名称-------------");
+        System.out.println(Constants.LiveWatermark_Enum.OBS_RIGHT_UP.name());
+        System.out.println(Constants.LiveWatermark_Enum.OBS_RIGHT_UP);
+        System.out.println(Constants.LiveWatermark_Enum.OBS_RIGHT_UP.toString());
     }
 }
