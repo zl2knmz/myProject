74e81358da9c6867bfb508de6cd34d4e2d94104b
zl <837870929@qq.com>
2022-05-07 11:26:46 +0800

    FeatureUtils

---
+++ maven1/src/main/java/com/cloud/util/FeatureUtils.java
@@ -0,0 +1,50 @@
+package com.cloud.util;
+
+/**
+ * @author zl
+ * @date 2022/5/7 11:24
+ */
+public class FeatureUtils {
+    /**
+     * 设置活动feature值
+     *
+     * @param value    数据库featured值
+     * @param open     0-关掉 1-开启
+     * @param featured 开关位数 22-设置回放，27-活动号会员回放
+     */
+    public static int getActivityFeatured(int value, int featured, boolean open) {
+        int result = 0;
+        int maxLength = 27;
+
+        if (open) {
+            // 开启featured
+            char[] ats = new char[maxLength];
+            for (int i = 0; i < ats.length; i++) {
+                ats[i] = '0';
+            }
+            ats[maxLength - featured] = '1';
+            int a = Integer.parseInt(new String(ats), 2);
+            result = (value | a);
+            System.out.println("open featured=" + featured);
+        } else {
+            // 关feature
+            char[] ats = new char[maxLength];
+            for (int i = 0; i < ats.length; i++) {
+                ats[i] = '1';
+            }
+            ats[maxLength - featured] = '0';
+            int a = Integer.parseInt(new String(ats), 2);
+            result = (value & a);
+            System.out.println("close featured=" + featured);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // 2048 22 2099200 22=2097152 27=67108864
+        int a = getActivityFeatured(0, 27, true);
+        System.out.println(a);
+        int b = getActivityFeatured(0, 27, false);
+        System.out.println(b);
+    }
+}
