2692f75db1e8093523371aa5564e84efff76d454
zl <837870929@qq.com>
2022-05-16 19:33:55 +0800

    xml cdata

--- maven1/src/main/java/com/cloud/util/XmlUtils.java
+++ maven1/src/main/java/com/cloud/util/XmlUtils.java
@@ -1,6 +1,7 @@
 package com.cloud.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -67,15 +68,60 @@ public class XmlUtils {
         return xml;
     }
 
+    /**
+     * @param value <![CDATA[内容]]>
+     */
+    public static String updateXmlNodeCDATA(String xml, String nodeName, String value) {
+        if (null == value) {
+            value = "";
+        }
+        if (StringUtils.isBlank(xml)) {
+            Document document = DocumentHelper.createDocument();
+            Element root = document.addElement("Setting");
+            Element first = root.addElement(nodeName);
+            first.addCDATA(value);
+            xml = document.getRootElement().asXML();
+            return xml;
+        } else {
+            // 把文件解析成document树
+            Document document = null;
+            try {
+                document = DocumentHelper.parseText(xml);
+                // 获取根节点
+                Element root = document.getRootElement();
+                if (null != root.element(nodeName)) {
+                    root.element(nodeName).setText("");
+                    root.element(nodeName).addCDATA(value);
+                } else {
+                    Element first = root.addElement(nodeName);
+                    first.setText("");
+                    first.addCDATA(value);
+                }
+                xml = document.getRootElement().asXML();
+                return xml;
+            } catch (DocumentException e) {
+                log.error("updateXmlNode", e);
+            }
+        }
+        return xml;
+    }
+
     public static void main(String[] args) {
-        String xml = "<Setting><IsAdmin>True</IsAdmin><PlaybackMemberSN>1,2</PlaybackMemberSN></Setting>";
-        String nodeName = "PlaybackMemberSN";
+        String xml = "<Setting><IsAdmin>True</IsAdmin><PlaybackMemberSN>1,2</PlaybackMemberSN><HdxTags><![CDATA[IT互联网,创业]]></HdxTags></Setting>";
+        String nodeName = "HdxTags";
         String value = getXmlNodeValue(xml, nodeName);
         System.out.println(value);
 
         String xml1 = "<Setting><IsAdmin>True</IsAdmin></Setting>";
-        String nodeName1 = "PlaybackMemberSN";
-        String value1 = "1";
-        System.out.println(updateXmlNode(xml1, nodeName1, value1));
+        String nodeName1 = "HdxTags";
+        String value1 = "IT互联网--,创业--";
+        String xml2 = updateXmlNodeCDATA(xml1, nodeName1, value1);
+        System.out.println(xml2);
+
+//        String xml3 = "<Setting><IsAdmin>True</IsAdmin><HdxTags><![CDATA[IT互联网--,创业--]]></HdxTags></Setting>";
+        String nodeName2 = "HdxTags";
+        String value2 = getXmlNodeValue(xml2, nodeName2);
+        System.out.println(value2);
     }
+
 }
