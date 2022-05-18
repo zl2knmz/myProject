b05f8e2c9d63cc51a86210735506a3b5b41b64b7
Zhongliang <837870929@qq.com>
2022-05-18 22:46:27 +0800

    1

--- maven1/src/main/java/com/cloud/offer/day3/ThreadPoolDemo.java
+++ maven1/src/main/java/com/cloud/offer/day3/ThreadPoolDemo.java
@@ -13,9 +13,32 @@ public class ThreadPoolDemo {
         // 线程池创建单线程
         ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
                 .setNameFormat("demo-pool-%d").build();
-        ExecutorService singleThreadPool = new ThreadPoolExecutor(1, 1,
-                0L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+
+        /**
+         * 线程池7个参数
+         *@param corePoolSize the number of threads to keep in the pool, even
+         *        if they are idle, unless {@code allowCoreThreadTimeOut} is set
+         * @param maximumPoolSize the maximum number of threads to allow in the
+         *        pool
+         * @param keepAliveTime when the number of threads is greater than
+         *        the core, this is the maximum time that excess idle threads
+         *        will wait for new tasks before terminating.
+         * @param unit the time unit for the {@code keepAliveTime} argument
+         * @param workQueue the queue to use for holding tasks before they are
+         *        executed.  This queue will hold only the {@code Runnable}
+         *        tasks submitted by the {@code execute} method.
+         * @param threadFactory the factory to use when the executor
+         *        creates a new thread
+         * @param handler the handler to use when execution is blocked
+         *        because the thread bounds and queue capacities are reached
+         */
+        ExecutorService singleThreadPool = new ThreadPoolExecutor(1,
+                1,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>(1024),
+                namedThreadFactory,
+                new ThreadPoolExecutor.AbortPolicy());
 
         singleThreadPool.execute(() -> System.out.println(Thread.currentThread().getName()));
         singleThreadPool.shutdown();
