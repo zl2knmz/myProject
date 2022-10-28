d193f24349d4d44dc113d9a632fc4ec25ed3c9a0
zl <837870929@qq.com>
2022-10-28 17:34:03 +0800

    stream

--- maven1/src/main/java/com/cloud/test/JavaStreamTest.java
+++ maven1/src/main/java/com/cloud/test/JavaStreamTest.java
@@ -1,9 +1,14 @@
 package com.cloud.test;
 
+import lombok.Data;
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -75,4 +80,159 @@ public class JavaStreamTest {
         System.out.println("random forEach");
         random.ints().limit(10).forEach(System.out::println);
     }
+
+    /**
+     * boolean anyMatch(Predicate<? super T> predicate); // 元素是否有任意一个满足条件
+     * boolean allMatch(Predicate<? super T> predicate); // 元素是否都满足条件
+     * boolean noneMatch(Predicate<? super T> predicate); // 元素是否都不满足条件
+     */
+    @Test
+    public void match() {
+        boolean b = Stream.of("1", "3", "3", "4", "5", "1", "7")
+                .map(Integer::parseInt)
+                //.allMatch(s -> s > 0)
+                //.anyMatch(s -> s >4)
+                .noneMatch(s -> s > 4);
+        System.out.println(b);
+    }
+
+    /**
+     * 如果我们需要找到某些数据，可以使用find方法来实现。
+     */
+    @Test
+    public void find() {
+        Optional<String> first = Stream.of("10", "3", "3", "4", "5", "1", "7").findFirst();
+        System.out.println(first.get());
+
+        Optional<String> any = Stream.of("111", "3", "3", "4", "5", "1", "7").findAny();
+        System.out.println(any.get());
+    }
+
+    /**
+     * 最大值、最小值
+     */
+    @Test
+    public void max() {
+        Optional<Integer> max = Stream.of("1", "3", "3", "4", "5", "1", "7")
+                .map(Integer::parseInt)
+                .max((o1, o2) -> o1 - o2);
+        System.out.println(max.get());
+
+        Optional<Integer> min = Stream.of("1", "3", "3", "4", "5", "1", "7")
+                .map(Integer::parseInt)
+                .min((o1, o2) -> o1 - o2);
+        System.out.println(min.get());
+
+    }
+
+    /**
+     * 如果需要将所有数据归纳得到一个数据，可以使用reduce方法。
+     */
+    @Test
+    public void reduce() {
+        Integer sum = Stream.of(4, 5, 3, 9)
+                // identity默认值
+                // 第一次的时候会将默认值赋值给x
+                // 之后每次会将 上一次的操作结果赋值给x y就是每次从数据中获取的元素
+                .reduce(0, (x, y) -> {
+                    System.out.println("x=" + x + ",y=" + y);
+                    return x + y;
+                });
+        System.out.println(sum);
+
+        // 获取 最大值
+        Integer max = Stream.of(4, 5, 3, 9)
+                .reduce(0, (x, y) -> {
+                    return x > y ? x : y;
+                });
+        System.out.println(max);
+    }
+
+    /**
+     * 在实际开发中我们经常会将map和reduce一块来使用。
+     */
+    @Test
+    public void mapReduce() {
+        // 1.求出所有年龄的总和
+        Integer sumAge = Stream.of(
+                        new Person("张三", 18)
+                        , new Person("李四", 22)
+                        , new Person("张三", 13)
+                        , new Person("王五", 15)
+                        , new Person("张三", 19)
+                ).map(Person::getAge) // 实现数据类型的转换
+                .reduce(0, Integer::sum);
+        System.out.println(sumAge);
+
+        // 2.求出所有年龄中的最大值
+        Integer maxAge = Stream.of(
+                        new Person("张三", 18)
+                        , new Person("李四", 22)
+                        , new Person("张三", 13)
+                        , new Person("王五", 15)
+                        , new Person("张三", 19)
+                ).map(Person::getAge) // 实现数据类型的转换，符合reduce对数据的要求
+                .reduce(0, Math::max); // reduce实现数据的处理
+        System.out.println(maxAge);
+
+        // 3.统计 字符 a 出现的次数
+        Integer count = Stream.of("a", "b", "c", "d", "a", "c", "a")
+                .map(ch -> "a".equals(ch) ? 1 : 0)
+                .reduce(0, Integer::sum);
+        System.out.println(count);
+
+    }
+
+    /**
+     * 如果需要将Stream中的Integer类型转换成int类型，可以使用mapToInt方法来实现。
+     */
+    @Test
+    public void match4() {
+        // Integer占用的内存比int多很多，在Stream流操作中会自动装修和拆箱操作
+        Integer arr[] = {1, 2, 3, 5, 6, 8};
+        Stream.of(arr)
+                .filter(i -> i > 0)
+                .forEach(System.out::println);
+        System.out.println("---------");
+        // 为了提高程序代码的效率，我们可以先将流中Integer数据转换为int数据，然后再操作
+        IntStream intStream = Stream.of(arr)
+                .mapToInt(Integer::intValue);
+        intStream.filter(i -> i > 3)
+                .forEach(System.out::println);
+
+    }
+
+    /**
+     * 如果有两个流，希望合并成为一个流，那么可以使用Stream接口的静态方法concat。
+     */
+    @Test
+    public void concat() {
+        Stream<String> stream1 = Stream.of("a", "b", "c");
+        Stream<String> stream2 = Stream.of("x", "y", "z");
+        // 通过concat方法将两个流合并为一个新的流
+        Stream.concat(stream1, stream2).forEach(System.out::println);
+
+    }
+
+
+    /**
+     *
+     */
+    @Test
+    public void match8() {
+
+    }
+
+
+    @Data
+    static class Person {
+        String name;
+        Integer age;
+
+        public Person(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
 }
