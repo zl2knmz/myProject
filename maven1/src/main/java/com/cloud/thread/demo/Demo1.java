f700aa87281b6d75759e47db7563a0c62063a593
Zhongliang <837870929@qq.com>
2023-03-07 23:59:18 +0800

    1

--- maven1/src/main/java/com/cloud/thread/demo/Demo1.java
+++ maven1/src/main/java/com/cloud/thread/demo/Demo1.java
@@ -4,7 +4,9 @@ import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Vector;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -34,13 +36,26 @@ public class Demo1 {
         return threadPoolExecutor;
     }
 
+    /**
+     * 获取线程安全的List我们可以通过Vector、Collections.synchronizedList()方法和CopyOnWriteArrayList三种方式
+     * 读多写少的情况下，推荐使用CopyOnWriteArrayList方式
+     * 读少写多的情况下，推荐使用Collections.synchronizedList()的方式
+     * ————————————————
+     * 原文链接：https://blog.csdn.net/weixin_45668482/article/details/117396603
+     */
     public List<Integer> sumSync(int size) {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             list.add(i);
         }
 
-        List<Integer> sumList = new ArrayList<>();
+        // ArrayList 线程不安全
+//        List<Integer> sumList = new ArrayList<>();
+        // 线程安全的List： Vector、Collections.synchronizedList()方法和CopyOnWriteArrayList三种方式
+        Vector<Integer> sumList = new Vector<>();
+//        CopyOnWriteArrayList<Integer> sumList = new CopyOnWriteArrayList<>();
+//        List<Integer> sumList = Collections.synchronizedList(new ArrayList<>());
+
 //        List<List<Integer>> parts = SplitListUtils.split(list, 100);
         List<List<Integer>> parts = Lists.partition(list, 10);
 
