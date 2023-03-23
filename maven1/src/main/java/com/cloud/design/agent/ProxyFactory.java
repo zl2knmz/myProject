12ae06d5e0311167dff430bafc5493bd07e04402
zl <837870929@qq.com>
2023-03-23 16:20:47 +0800

    1

--- maven1/src/main/java/com/cloud/design/agent/ProxyFactory.java
+++ maven1/src/main/java/com/cloud/design/agent/ProxyFactory.java
@@ -12,7 +12,7 @@ import java.lang.reflect.Proxy;
  */
 public class ProxyFactory {
 
-    public static Object getAgent(Service target, Aop aop){
+    public static Object getAgent(Service target, Aop aop) {
         // 返回生成的动态代理对象
         return Proxy.newProxyInstance(
                 // 类加载器
@@ -21,14 +21,13 @@ public class ProxyFactory {
                 target.getClass().getInterfaces(),
                 // 代理功能实现
                 new InvocationHandler() {
+                    /**
+                     * @param proxy 生成的代理对象
+                     * @param method 正在被调用的目标方法buy(),show()
+                     * @param args 目标方法的参数
+                     */
                     @Override
-                    public Object invoke(
-                            // 生成的代理对象
-                            Object proxy,
-                            // 正在被调用的目标方法buy(),show()
-                            Method method,
-                            // 目标方法的参数
-                            Object[] args) throws Throwable {
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                         Object obj = null;
                         try {
                             // 切面
