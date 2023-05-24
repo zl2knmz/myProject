b8dbf912e8bed27b8d558bcffa34e20b36f91bde
zl <837870929@qq.com>
2023-05-24 17:12:07 +0800

    1

--- maven1/src/main/java/com/cloud/thread/jmm/VolatileTest.java
+++ maven1/src/main/java/com/cloud/thread/jmm/VolatileTest.java
@@ -1,6 +1,10 @@
 package com.cloud.thread.jmm;
 
 /**
+ * volatile写之前的操作，都禁止重排序到volatile之后
+ * volatile读之后的操作，都禁止重排序到volatile之前
+ * volatile写之后volatile读，禁止重排序
+ *
  * @author zl
  * @date 2023/5/4 23:15
  */
