cf9d37be9863ca3846bdc715de2ffcd70039f39b
zl <837870929@qq.com>
2023-06-03 17:59:34 +0800

    1

---
+++ maven1/src/main/java/com/cloud/thread/cas/CASDemo.java
@@ -0,0 +1,16 @@
+package com.cloud.thread.cas;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author zl
+ * @date 2023/6/3 17:43
+ */
+public class CASDemo {
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+
+        System.out.println(atomicInteger.compareAndSet(5, 2022)+"\t"+atomicInteger.get());
+        System.out.println(atomicInteger.compareAndSet(6, 2022)+"\t"+atomicInteger.get());
+    }
+}
